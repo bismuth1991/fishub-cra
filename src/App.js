e0e41e5ccdb8fbe7544b5fca2ee6916e513722b8
Bao Tran <bismuth@pop-os.localdomain>
2020-01-19 23:16:55 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello CRA with Tailwindcss!
+      </h1>
     </div>
   );
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        fishub: {
+          green: '#94d31b',
+          brand: '#e2ebf9',
+          'nav-auth': '#0c385a',
+          'nav-main': '#15629d',
+          primary: '#666',
+          secondary: '#6c757d',
+          error: '#e8890c',
+        },
+      },
+      fontSize: {
+        brandXl: '2.6rem',
+      },
+      opacity: {
+        '80': '.8',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
