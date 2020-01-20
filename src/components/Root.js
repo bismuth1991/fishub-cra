96b35072a9ad7b379393d4488b787a42071c3220
Bao Tran <bismuth@pop-os.localdomain>
2020-01-20 11:50:49 -0500

    Add UserProvider to Root

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,12 +2,15 @@ import React from 'react';
 import App from './App';
 import AuthTokenProvider from './context/AuthTokenContext';
 import AxiosProvider from './context/AxiosContext';
+import UserProvider from './context/UserContext';
 
 const Root = () => {
   return (
     <AxiosProvider>
       <AuthTokenProvider>
-        <App />
+        <UserProvider>
+          <App />
+        </UserProvider>
       </AuthTokenProvider>
     </AxiosProvider>
   );
